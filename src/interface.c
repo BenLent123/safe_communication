4d5b58ee7ef925248b2d8d37e1b4a807bc51d877
ben_laptop <lentschigben@gmail.com>
2025-06-03 17:08:53 +0200

    smol change

--- src/interface.c
+++ src/interface.c
@@ -7,6 +7,7 @@
 #include "client.h"
 #include "common.h"
 
+
 int main(int argc, char *argv[]) {
     AES_KEY encrpytionKey, decryptionKey;
     char cmd;
@@ -16,10 +17,10 @@ int main(int argc, char *argv[]) {
         
     do {
         do {
-            printf("enter the encryption key, both parties need the same key (16 characters long).\n");
+            printf("enter the encryption key, both parties need the same key (16 characters long, only first 16 are taken ).\n");
             scanf("%16s", key);
             if(strlen(key) != 16){
-                printf("key was not long enough\n");
+                printf("key was not correct in length - too short\n");
             }
         } while(strlen(key) != 16);
         SetEncryptionKeys((unsigned char*)key, &encrpytionKey, &decryptionKey);
@@ -34,23 +35,8 @@ int main(int argc, char *argv[]) {
                 break;
             case 'j':
                 printf("You chose to join.\n");
-                do {
-                    printf("input your server IP (ex = 127.0.0.1 -> localhost), leave empty for localhost.\n");
-                    fgets(serverIpAddress, sizeof(serverIpAddress), stdin);
-                    // Remove newline if present
-                    size_t ipLen = strlen(serverIpAddress);
-                    if (ipLen > 0 && serverIpAddress[ipLen-1] == '\n') {
-                        serverIpAddress[ipLen-1] = '\0';
-                        ipLen--;
-                    }
-                    if (ipLen == 0) {
-                        strcpy(serverIpAddress, "127.0.0.1");
-                        ipLen = strlen(serverIpAddress);
-                    }
-                    if (ipLen < 7 || ipLen > 15) {
-                        printf("Invalid IP length. Please enter a valid IPv4 address.\n");
-                    }
-                } while (strlen(serverIpAddress) < 7 || strlen(serverIpAddress) > 15);
+                printf("input your server IP (ex = 127.0.0.1 -> localhost), leave empty for localhost.\n");
+                scanf(" %s", serverIpAddress);
                 printf("input your server port (ex = 8080).\n");
                 scanf(" %d", &serverPort);
                 ClientMainFunc(serverIpAddress, serverPort, &encrpytionKey, &decryptionKey);
